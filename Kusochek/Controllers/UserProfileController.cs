6ee503357ef07321b0b69890437052e62ede8590
makeitokay <andrey.mu11@yandex.ru>
2024-03-09 15:52:35 +0300

    test controller

---
+++ Kusochek/Controllers/UserProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kusochek;
+
+[Route("profile")]
+public class UserProfileController : ControllerBase
+{
+	private readonly ICustomerRepository _customerRepository;
+
+	public UserProfileController(ICustomerRepository customerRepository)
+	{
+		_customerRepository = customerRepository;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetProfileAsync()
+	{
+		return Ok("Hello world!");
+	}
+}
