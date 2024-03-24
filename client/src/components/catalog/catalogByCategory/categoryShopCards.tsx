801a807ef2fd86cff99e8898171fbf3178dc2158
korol <akorolev@edu.hse.ru>
2024-03-24 23:13:27 +0300

    fix items by categories

--- client/src/components/catalog/catalogByCategory/categoryShopCards.tsx
+++ client/src/components/catalog/catalogByCategory/categoryShopCards.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Product} from "../../../types/product";
+import {Product, ProductCard} from "../../../types/product";
 import {fetchListProductsByCategory} from "../../../HTTPRequests/fetchListProductsByCategory";
 import ShopCard from "../shopCards/ShopCard";
 import "../../../styles/caruselStyle.css"
@@ -7,29 +7,33 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import "../../../styles/CardPageStyles.css"
 import CustomLink from "../../CustomLink";
+import {getAllItemsRequest} from "../../../HTTPRequests/store/getItemsRequest";
+import {toast} from "react-toastify";
 interface CategoryShopCardsProps {
     category: string,
     nameCategory: string
 }
 
 const CategoryShopCards = ({category, nameCategory}: CategoryShopCardsProps) => {
-    const [items, setItems] = useState<Product[]>([])
+    const [items, setItems] = useState<ProductCard[]>([])
     const params = new URLSearchParams();
+    const notifyError = (message: string) => toast.error(message);
     params.append('categories', nameCategory);
     useEffect(() => {
-        fetchListProductsByCategory({category}).then(data => {
-                let array: Product[] = []
-                for (let item of data.results) {
-                    array.push({
-                        name: item.name,
-                        image: item.images[0].url,
-                        price: item.price.formattedValue,
-                        id: item.allArticleCodes[0]
-                    })
-                }
-                setItems(array)
-            }
-        )
+        getAllItemsRequest(undefined, undefined, [nameCategory]).then((data)=> setItems(data)).catch(()=>notifyError("Не удалось получить товары по категории"))
+        // fetchListProductsByCategory({category}).then(data => {
+        //         let array: Product[] = []
+        //         for (let item of data.results) {
+        //             array.push({
+        //                 name: item.name,
+        //                 image: item.images[0].url,
+        //                 price: item.price.formattedValue,
+        //                 id: item.allArticleCodes[0]
+        //             })
+        //         }
+        //         setItems(array)
+        //     }
+        // )
     }, []);
     const responsive = {
         superLargeDesktop: {
@@ -50,16 +54,16 @@ const CategoryShopCards = ({category, nameCategory}: CategoryShopCardsProps) => 
             items: 2
         }
     };
-    if (items.length === 0) {
-        return <></>
-    }
+    // if (items.length === 0) {
+    //     return <></>
+    // }
     return (
         <div style={{paddingBottom: "10vh"}}>
             <CustomLink to={`/shop?${params.toString()}`} style={{border:0, margin:"1rem"}}>
                 <h2>{nameCategory}</h2>
             </CustomLink>
             <Carousel responsive={responsive}>
-                {/*{items.map(obj => (<ShopCard route ={`items/${obj.id}`} className="catalogCard" item={obj}/>))}*/}
+                {items.map(obj => (<ShopCard route ={`items/${obj.id}`} className="catalogCard" item={obj}/>))}
             </Carousel>
         </div>
     );
